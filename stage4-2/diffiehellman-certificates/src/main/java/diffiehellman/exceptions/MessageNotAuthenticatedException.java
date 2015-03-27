fcaa42f75f75ff2e5c1e7b7c39f7a3685144e260
Duknust <duartenuno1992@hotmail.com>
2015-03-27 20:32:40 +0000

    [Add] Stage 4-2 certificates validation

---
+++ stage4-2/diffiehellman-certificates/src/main/java/diffiehellman/exceptions/MessageNotAuthenticatedException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diffiehellman.exceptions;
+
+/**
+ *
+ * @author duarteduarte
+ */
+public class MessageNotAuthenticatedException extends Exception {
+
+    public MessageNotAuthenticatedException(String wrong_mac) {
+    }
+
+}
